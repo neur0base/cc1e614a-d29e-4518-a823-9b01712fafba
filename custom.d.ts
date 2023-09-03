01f01ddd3ea2b1270d3859429b1248f3280b3105
Some One <some@one.com>
2023-09-03 16:19:31 +0900

    Removed unused files.

--- custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.svg' {
-  const content: any;
-  export default content;
-}
-
-declare module '@/assets/images/avatar_failback.png';
--- outer_load_test.js
+++
@@ -1,7 +0,0 @@
-require('ts-node').register();
-
-const routing = require('./src/routing/index').default;
-
-console.log(routing);
-
-// $ node outer_load_test.js
