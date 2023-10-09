43afd83cf0fd2c6299912218919c51aa0a78a728
Some One <some@one.com>
2023-10-09 16:41:18 +0900

    Fixed config/themeColors.

--- src/config/themeColors.ts
+++ src/config/themeColors.ts
@@ -1,7 +1,19 @@
-import { ThemeColors } from "@neur0base/app-sdk-core";
+export declare type ThemeColors = {
+    mainColor: string;
+    baseColor: string;
+    textColor: string;
+    accentColor1: string;
+    accentColor2: string;
+    accentColor3: string;
+};
 
 const themeColors: ThemeColors = {
-    
+    mainColor: "#0E0F0F",
+    baseColor: "#FFFFFF",
+    textColor: "#0E0F0F",
+    accentColor1: "#4CD471",
+    accentColor2: "#FF5247",
+    accentColor3: "#FFB323",
 }
 
 export default themeColors;
