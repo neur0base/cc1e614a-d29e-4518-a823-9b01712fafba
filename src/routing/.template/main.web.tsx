4b3c6ff4447aa3687b48c09e371e69cc28b9a3dd
Some One <some@one.com>
2024-01-02 21:43:35 +0900

    Added src/routing/.template/

---
+++ src/routing/.template/main.web.tsx
@@ -0,0 +1,48 @@
+// @ts-nocheck
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    DynamicRouter,
+} from '@neur0base/app-sdk-core';
+import { initialize } from '@echopf/sdk';
+
+import '../../web/index.css';
+import '../../web/app.css';
+
+import instances from '../../config/instances';
+import themeColors from '../../config/themeColors';
+import locales from '../../locales';
+import * as components from '../../components/views';
+import * as mockModels from '../../components/mockModels';
+import * as models from '../../components/models';
+import { appConfig, layout, mainRouteID } from './app';
+import routes from './routes';
+
+const App = () => {
+
+    // [ToDo] Set secureDomain & appID & appKey by env variables
+    const backendClient = initialize("test", "test", "test");
+
+    return (
+        <DynamicRouter
+            config={appConfig}
+            layout={layout}
+            backend={backendClient}
+            models={process.env.ENVIRONMENT === "production" ? models : mockModels}
+            instances={instances}
+            components={components}
+            routes={routes}
+            mainRouteID={mainRouteID}
+            OuterProvider={(props) => {
+                return (<>{props.children}</>);
+            }}
+            theme={{
+                colors: themeColors
+            }}
+            locales={locales}
+        />
+    );
+};
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
