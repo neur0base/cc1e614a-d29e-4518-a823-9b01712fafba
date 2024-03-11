a9121462fa3a4d116bec210e2737f9bb4529113f
Some One <some@one.com>
2024-03-11 11:29:03 +0900

    Changed structure of locales & colors.

--- src/config/themeColors.ts
+++ src/config/themeColors.ts
@@ -1,33 +1,21 @@
-export declare type ThemeColors = {
-    mainColor: string;
-    backgroundColor: string;
-    accentColor: string;
-    accentColor2: string;
-    textColor: string;
-    linkColor: string;
-    successColor: string;
-    errorColor: string;
-    waringColor: string;
-    placeholderTextColor: string;
-    disabledColor: string;
-    borderColor: string;
-    caretColor: string;
-};
+import { ThemeColors } from "@neur0base/app-sdk-core";
 
 const themeColors: ThemeColors = {
-    mainColor: "#424242",
-    accentColor: "#616161",
-    accentColor2: "#757575",
-    backgroundColor: "#CDCDCD",
-    textColor: "#E0E0E0",
-    linkColor: "#1A237E",
-    successColor: "#1B5E20",
-    errorColor: "#B71C1C",
-    waringColor: "#FF6F00",
-    placeholderTextColor: "#757575",
-    disabledColor: "#BDBDBD",
-    borderColor: "#BDBDBD",
-    caretColor: "#FF6F00",
+    common: {
+        primaryColor: "#333333",
+        secondaryColor: "#616161",
+        backgroundColor: "#EFEFEF",
+        textColor: "#E0E0E0",
+        linkColor: "#1A237E",
+        inputColor: "#E0E0E0",
+        disabledColor: "#BDBDBD",
+        successColor: "#1B5E20",
+        warningColor: "#FF6F00",
+        errorColor: "#B71C1C",
+    },
+    components: {
+
+    },
 };
 
 export default themeColors;
--- src/routing/.template/main.web.tsx
+++ src/routing/.template/main.web.tsx
@@ -5,18 +5,13 @@ import {
     DynamicRouter,
 } from '@neur0base/app-sdk-core';
 import { initialize } from '@echopf/sdk';
-
-import '../../web/index.css';
-import '../../web/app.css';
-
-import instances from '../../config/instances';
-import themeColors from '../../config/themeColors';
-import locales from '../../locales';
-import * as components from './routes/index';
-import * as mockModels from '../../components/mockModels';
-import * as models from '../../components/models';
-import { appConfig, layout, mainRouteID } from './app';
+import instances from '@/src/config/instances';
+import themeColors from '@/src/config/themeColors';
+import locales from '@/src/locales';
+import * as models from '@/src/components/models';
+import appConfig from './app';
 import routes from './routes';
+import * as components from './routes/index';
 
 const App = () => {
 
@@ -26,21 +21,16 @@ const App = () => {
     return (
         <DynamicRouter
             config={appConfig}
-            layout={layout}
             backend={backendClient}
-            models={process.env.ENVIRONMENT === "production" ? models : mockModels}
+            models={models}
             instances={instances}
             components={components}
             routes={routes}
-            mainRouteID={mainRouteID}
-            OuterProvider={(props) => {
-                return (<>{props.children}</>);
-            }}
             theme={{
                 colors: themeColors
             }}
             locales={locales}
-        />
+            />
     );
 };
 
